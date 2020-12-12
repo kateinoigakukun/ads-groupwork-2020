2b344e8770be26300008b14a44f765b8872e4af1
Yuta Saito <kateinoigakukun@gmail.com>
2020-12-12 11:40:49 +0900

    Implement viterbi code

--- grpwk20/dec.c
+++ grpwk20/dec.c
@@ -1,12 +1,15 @@
 #include "grpwk20.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define SEGMENT_SIZE SR_SIZE
 // x = 200000/(25 - log_4(x))
 // log_4 (x) ≒ 7
 #define SEGMENT_INDEX_SIZE 7
-#define HEADER_SIZE SEGMENT_INDEX_SIZE
+#define SEGMENT_CONSTRAINT_LENGTH 3
+#define SEGMENT_INDEX_STATE_COUNT (SEGMENT_CONSTRAINT_LENGTH - 1) * 2
+#define HEADER_SIZE SEGMENT_INDEX_SIZE + SEGMENT_CONSTRAINT_LENGTH - 1
 
 unsigned decodeUInt(unsigned char value) {
   switch (value) {
@@ -36,6 +39,91 @@ int readSegmentIndex(FILE *fp) {
     value += decodeUInt(loaded) << (2 * digits);
   }
   return value;
+}
+
+int hammingDistance(int lhs, int rhs) {
+  int xored = lhs ^ rhs;
+  int distance = 0;
+  while (xored) {
+    xored &= xored - 1;
+    distance++;
+  }
+  return distance;
+}
+
+typedef struct {
+  int output;
+  int nextState;
+} transition_output_t;
+
+transition_output_t convolutionTransition(int input, int state) {
+  int inputs[SEGMENT_CONSTRAINT_LENGTH];
+  inputs[0] = input;
+  int registerSize = SEGMENT_CONSTRAINT_LENGTH - 1;
+  for (int i = 0; i < registerSize; i++) {
+    inputs[1 + i] = (state >> (registerSize - i - 1)) & 0x1;
+  }
+
+  int nextState = (state >> 1) | (input << (registerSize - 1));
+  int c0 = (inputs[0] + inputs[2]) & 0x1;
+  int c1 = (inputs[0] + inputs[1] + inputs[2]) & 0x1;
+
+  return (transition_output_t){
+    .output = (c1 << 1) + c0, .nextState = nextState
+  };
+}
+
+typedef struct {
+  int fromState;
+  int minPathMetric;
+  int inputBit;
+  bool isActivated;
+} state_node_t;
+
+int decodeViterbi(FILE *fp) {
+  state_node_t paths[HEADER_SIZE + 1][SEGMENT_INDEX_STATE_COUNT];
+  paths[0][0].fromState = 0;
+  paths[0][0].minPathMetric = 0;
+  paths[0][0].isActivated = true;
+
+  for (int time = 0; time < HEADER_SIZE; time++) {
+    
+    char inputBitChar = getc(fp);
+    if (inputBitChar == '\n')
+      return -1;
+    int expectedOutput = decodeUInt(inputBitChar);
+
+    for (int state = 0; state < SEGMENT_INDEX_STATE_COUNT; state++) {
+      if (!paths[time][state].isActivated) continue;
+
+      bool onlyAcceptZero = time > SEGMENT_INDEX_SIZE;
+      for (int possibleInput = 0; possibleInput <= (onlyAcceptZero ? 0 : 1); possibleInput++) {
+        transition_output_t output = convolutionTransition(possibleInput, state);
+        
+        int branchMetric = hammingDistance(expectedOutput, output.output);
+        int pathMetric = paths[time][state].minPathMetric + branchMetric;
+        
+        state_node_t *nextStates = paths[time + 1];
+        bool shouldUpdate = (nextStates[output.nextState].isActivated) &&
+        nextStates[output.nextState].minPathMetric > pathMetric;
+        
+        if (!nextStates[output.nextState].isActivated || shouldUpdate) {
+          nextStates[output.nextState].minPathMetric = pathMetric;
+          nextStates[output.nextState].fromState = state;
+          nextStates[output.nextState].inputBit = possibleInput;
+        }
+        nextStates[output.nextState].isActivated = true;
+      }
+    }
+  }
+
+  int lastState = 0;
+  int result = 0;
+  for (int time = HEADER_SIZE - 1; 0 <= time; time--) {
+    result |= paths[time][lastState].inputBit << (HEADER_SIZE - time - 2);
+    lastState = paths[time][lastState].fromState;
+  }
+  return result;
 }
 
 void dec(void) {
@@ -85,6 +173,13 @@ void dec(void) {
 }
 
 int main(void) {
-  dec();
+  FILE *sourceFile;
+  if ((sourceFile = fopen("testdata", "r")) == NULL) {
+    fprintf(stderr, "cannot open %s\n", SEQDATA);
+    exit(1);
+  }
+  int result = decodeViterbi(sourceFile);
+  printf("%d\n", result);
+//  dec();
   return 0;
 }
--- grpwk20/enc.c
+++ grpwk20/enc.c
@@ -6,7 +6,8 @@
 // x = 200000/(25 - log_4(x))
 // log_4 (x) ≒ 7
 #define SEGMENT_INDEX_SIZE 7
-#define HEADER_SIZE SEGMENT_INDEX_SIZE
+#define SEGMENT_CONSTRAINT_LENGTH 3
+#define HEADER_SIZE SEGMENT_INDEX_SIZE + SEGMENT_CONSTRAINT_LENGTH - 1
 
 unsigned parseUInt(unsigned char upper, unsigned char lower) {
   return ((upper & 0x1) << 1) + (lower & 0x1);
@@ -28,16 +29,46 @@ unsigned char encodeUInt(unsigned value) {
   }
 }
 
-void writeIndex(unsigned int segmentIndex, FILE *fp) {
-  int shift = SEGMENT_INDEX_SIZE * 2;
-  while (shift > 0) {
-    shift--;
-    unsigned upperBit = (segmentIndex >> shift) & 0x1;
-    shift--;
-    unsigned lowerBit = (segmentIndex >> shift) & 0x1;
-    fputc(encodeUInt(upperBit * 2 + lowerBit), fp);
+int coder(int *reg, int bit) {
+  int registerSize = SEGMENT_CONSTRAINT_LENGTH - 1;
+  int inputs[SEGMENT_CONSTRAINT_LENGTH];
+  inputs[0] = bit;
+
+  for (int i = 0; i < registerSize; i++) {
+    inputs[1 + i] = (*reg >> (registerSize - i - 1)) & 0x1;
   }
+
+  int c0 = (inputs[0] + inputs[2]) & 0x1;
+  int c1 = (inputs[0] + inputs[1] + inputs[2]) & 0x1;
+
+  *reg = (*reg >> 1) | (bit << (registerSize - 1));
+  return (c1 << 1) + c0;
 }
+
+int encodeViterbi(int input, FILE *fp) {
+  int reg = 0;
+  for (int i = 0; i < SEGMENT_INDEX_SIZE; i++) {
+    int bit = (input >> (SEGMENT_INDEX_SIZE - i - 1)) & 0x1;
+    int output = coder(&reg, bit);
+    fputc(encodeUInt(output), fp);
+  }
+  for (int i = 0; i < SEGMENT_CONSTRAINT_LENGTH - 1; i++) {
+    int output = coder(&reg, 0);
+    fputc(encodeUInt(output), fp);
+  }
+  return 0;
+}
+
+//void writeIndex(unsigned int segmentIndex, FILE *fp) {
+//  int shift = SEGMENT_INDEX_SIZE * 2;
+//  while (shift > 0) {
+//    shift--;
+//    unsigned upperBit = (segmentIndex >> shift) & 0x1;
+//    shift--;
+//    unsigned lowerBit = (segmentIndex >> shift) & 0x1;
+//    fputc(encodeUInt(upperBit * 2 + lowerBit), fp);
+//  }
+//}
 
 int enc(void) {
   FILE *sourceFile;
@@ -56,7 +87,7 @@ int enc(void) {
   unsigned segmentCount = (ORGDATA_LEN / 2 + bodySize - 1) / bodySize;
 
   for (int segmentIdx = 0; segmentIdx < segmentCount; segmentIdx++) {
-    writeIndex(segmentIdx, outputFile);
+    encodeViterbi(segmentIdx, outputFile);
     for (int bodyIdx = 0; bodyIdx < bodySize; bodyIdx++) {
       unsigned char upperBit = getc(sourceFile);
       unsigned char lowerBit = getc(sourceFile);
@@ -71,6 +102,7 @@ int enc(void) {
 }
 
 int main(void) {
-  enc();
+//  enc();
+  encodeViterbi(28, stdout);
   return 0;
 }
