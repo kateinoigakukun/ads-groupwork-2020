ba34fd5298585052d1b8931ed85f63107870d0f0
Yuta Saito <kateinoigakukun@gmail.com>
2020-12-19 11:49:30 +0900

    Update harness script

--- grpwk20/harness.c
+++ grpwk20/harness.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    fprintf(stderr, "Usage: ./harness [iteration]\n");
+    exit(1);
+  }
+
   int success = 0;
-  int iteration = 100;
+  int iteration = atoi(argv[1]);
   for (int i = 0; i < iteration; i++) {
-    int status = system("bash -c \"./test.sh $(cat conf.txt | tail -n1)\" 2> /dev/null | grep -q hd=0");
+    int status = system("bash -c \"./test.sh $(cat conf.txt | tail -n1) 2> /dev/null | grep -q hd=0\"");
     if (status == 0) {
       success++;
     }
